7020159277001465ad9192cc60489119b4c280ca
rayne <pothong.r@gmail.com>
2025-10-04 20:23:34 -0700

    added sentence capitalization

---
+++ beginner algorithms/capSentence.js
@@ -0,0 +1,41 @@
+// Sentence Capitalization
+
+/* CHALLENGE
+Given a sentence containing two or more words, 
+return the equivalent of the sentence when capitalised. E.g
+  capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!' 
+*/
+
+
+// for each method
+
+function capSentence(text) {
+    let wordsArray = text.toLowerCase().split('')
+    let capsArray = []
+
+    wordsArray.forEach(word => {
+        capsArray.push(word[0].toUpperCase() + word.slice(1))
+    })
+    return capsArray.join('')
+ }
+
+ // map and slice method
+
+ function capSentence(text){
+    let wordsArray = text.toLowerCase().split('')
+    let capsArray = wordsArray.map(word => {
+        return word[0].toUpperCase() + word.slice(1)
+    })
+    return capsArray.join('')
+ }
+
+ // map and replace method
+
+ function capSentence(text){
+    let wordsArray = text.toLowerCase().split('')
+
+    let capsArray = wordsArray.map(word => {
+        return word.replace(word[0], word[0].toUpperCase)
+    })
+    return capsArray.join('')
+ }
