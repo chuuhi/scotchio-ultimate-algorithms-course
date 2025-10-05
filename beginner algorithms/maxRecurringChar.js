7596b711ed364fa6d17fa9ed0c76d2501a136042
rayne <pothong.r@gmail.com>
2025-10-04 18:55:05 -0700

    added most recurring character solution

---
+++ beginner algorithms/maxRecurringChar.js
@@ -0,0 +1,30 @@
+// Finding the Most Recurring Character
+
+/* CHALLENGE
+Given a string of text, find and return the most recurring character. 
+e.g maxRecurringChar('aabacada') // will return 'a'
+*/
+
+// loop through charMap object
+
+function maxRecurringChar(text) {
+    let charMap = {}
+    let maxCharValue = 0
+    let maxChar = ''
+
+    for (let char of text){
+        if (charMap.hasOwnProperty(char)){
+            charMap[char]++
+        }else{
+            charMap[char] = 1
+        }
+    }
+
+    for (let char in charMap){
+        if (charMap[char] > maxCharValue){
+            maxCharValue = charMap[char]
+            maxChar = char
+        }
+    }
+    return maxChar
+}
